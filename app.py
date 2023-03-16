7f42fdc7ecb3d9d8ca7033bfbc3954fd5b3e1095
Diksha4248 <88928334+Diksha4248@users.noreply.github.com>
2023-03-16 17:52:26 +0530

    home page changes

--- app.py
+++ app.py
@@ -182,6 +182,10 @@ def logout():
     session.pop('email', None)
     return render_template('home.html')
 
+@app.route ('/about')
+def about():
+    return render_template('about.html')
+
 
 
 if __name__ == '__main__':
